cbcdf40847f35df88b68f7cf97e25a1c5eee5a97
aaryansuri <aaryansuri0@gmail.com>
2023-03-11 13:40:21 +0000

    write proto to file

--- protobuf/src/main/java/com/grpc/protobuf/PersonDemo.java
+++ protobuf/src/main/java/com/grpc/protobuf/PersonDemo.java
@@ -2,21 +2,23 @@ package com.grpc.protobuf;
 
 import com.grpc.models.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PersonDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Person test = Person.newBuilder()
+        Person person = Person.newBuilder()
                 .setName("Aaryan")
                 .setAge(23)
                 .build();
 
-        Person test2 = Person.newBuilder()
-                .setName("Aaryan")
-                .setAge(23)
-                .build();
 
-        System.out.println(test.equals(test2));
+        Path path = Paths.get("sam.ser");
+        Files.write(path, person.toByteArray());
 
     }
 }
